ff3cdcc34ac3114fa9e1c5279ba0ef736fdec8f6
wangke <wangke19960129@gmail.com>
2016-08-30 20:34:31 +0800

    add 1001

--- poj/1001.cpp
+++ poj/1001.cpp
@@ -1,30 +1,88 @@
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include<string>
+#include<iostream>
+#include<cstring>
+#include<cstdio>
 
 using namespace std;
 
-string  mul(string a, string b){
-    string ans;
+char str[10];
+int a[99999],b[99999],ans[99999];
+int len_a,len_b,pos_dot = 0;
+int N;
 
-   // for(int i=a.begin() ; a<= )
+void mul(){
+    memset(ans,0,sizeof(ans));
+    int i,j;
+    for( i = 0; i< len_a ;i++){
+        for( j = 0; j < len_b;j++){
+            ans [ i + j ] = ans [ i + j ] + a[i] * b[j];
+            if(ans[i+j] > 9){
+                ans[i+j+1] = ans[i+j+1]  + ans[i+j] /10;
+                ans[i+j]  = ans[i+j]  %10;
+            }
+        }
+    }
 
-    return ans;
+    if( ans[len_a + len_b +1] != 0){
+        len_a = len_a + len_b +1;
+    }
+    else{
+        len_a = len_a + len_b;
+    }
+
+    for(i =0 ;i < len_a ;i++){
+        a[i] = ans[i];
+    }
+
+    return;
+}
+
+void print(int len ,int* D){
+    int first = len -1,last =0;
+
+    while(D[last] == 0 && last <= ((pos_dot) * N) ){
+        last  ++;
+    }
+
+    while(D[first] == 0 && first >= ((pos_dot) * N) ){
+        first  --;
+    }
+
+    for(int i=first; i>=last ;i--){
+        if(i  ==  pos_dot * N -1 ){
+            cout<<'.';
+        }
+        cout<<D[i];
+    }
+    cout<<endl;
 }
 
 int main(){
-    string  R;
-    int n;
-    int number_point;
-    while(cin>>R>>n){
-        cout<<R<<" "<<n<<endl;
-        reverse(R.begin(),R.end());
-        cout<<R<<" "<<n<<endl;
-        number_point = R.find_first_of('.');
-        R.erase('.');
-        cout<<R<<" "<<n<<endl;
+    int i,j;
+    while(cin>>str>>N){
+        pos_dot = 0;
+        memset(a,0,sizeof(a));
+        memset(b,0,sizeof(b));
+        if( N== 0){
+            cout<<"0"<<endl;
+            continue;
+        }
+        len_a = len_b = 6;
+        for(i=5,j = 0; i >= 0 ;i --){
+            if(str[i] != '.'){
+                a[j] = b[j] = str[i] - '0';
+                j++;
+            }
+            else{
+                pos_dot = j;
+                len_a -- ;
+                len_b -- ;
+            }
+        }
 
+        for(i = 1 ;i <N; i++){
+            mul();
+        }
+        print(len_a,a);
     }
     return 0;
 }
