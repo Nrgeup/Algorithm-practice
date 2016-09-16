5e5ccb6cd82dd7a597819073150088d4b5db9720
wangke <wangke19960129@gmail.com>
2016-09-16 21:31:22 +0800

    add 1029

---
+++ poj/1029.cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+
+//#define LOCAL
+
+using namespace std;
+
+
+
+
+int main(){
+    //从文件读入
+    #ifdef  LOCAL
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    #endif
+
+    /* TO DO  HERE!  */
+
+
+
+    return 0;
+}
