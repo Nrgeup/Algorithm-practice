f6881ef69ceecfbbd7f925633b04fc0ba72bc819
Nrgeup <wangkeanhui@163.com>
2016-10-10 23:29:00 +0800

    add 3020

---
+++ poj/3020.cpp
@@ -0,0 +1,27 @@
+/****************************************************************************\
+ *题意：
+ *
+ *思路：
+
+\****************************************************************************/
+#include<cstdio>   //尽量使用scanf和printf输入输出
+
+
+//#define LOCAL
+
+using namespace std;
+
+
+int main(){
+    //从文件读入
+    #ifdef  LOCAL
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    #endif
+
+    /* TO DO  HERE!  */
+	    
+
+
+    return 0;
+}
